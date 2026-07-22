ae56f4413a1aa9ae39a559d8b9aef0f7d8ce6c36
AslakOffi <aslakoffi@users.noreply.github.com>
2026-07-23 00:04:38 +0200

    refactor: use external bedrock-v/plugins framework

--- plugin.v
+++ plugin.v
@@ -1,27 +1,20 @@
 module vedrock_essentials
 
-import server.plugin
+import plugins
 
 pub struct Essentials {
-    plugin.Base
+    plugins.Base
 }
 
-pub fn (e Essentials) meta() plugin.Meta {
-    return plugin.Meta{
+pub fn (e Essentials) meta() plugins.Meta {
+    return plugins.Meta{
         name:    'Essentials'
         version: '0.1.0'
         authors: ['AslakOffi']
     }
 }
 
-pub fn (mut e Essentials) on_enable(mut api plugin.Api) {
-    e.log.info('Registering essential commands...')
-
-    // TODO: register commands once the plugin API is wired
-    // api.register_command(tpa_command())
-    // api.register_command(spawn_command())
-    // api.register_command(msg_command())
-
+pub fn (mut e Essentials) on_enable(mut api plugins.Api) {
     e.log.info('Essentials enabled')
 }
 
--- v.mod
+++ v.mod
@@ -3,5 +3,5 @@ Module {
     description: 'The essential plugin suite for Vedrock'
     version: '0.1.0'
     license: 'LGPL-3.0'
-    dependencies: []
+    dependencies: ['vedrock', 'plugins']
 }
